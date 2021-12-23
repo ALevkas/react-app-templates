87fdbb9d67361ad9b35d0b78d978e4486d27067c
allevkas <levkadev@gmail.com>
2021-12-23 16:45:35 +0300

    Add speed measure

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,14 @@
 const {merge} = require('webpack-merge')
 const commonConfig = require('./webpack.common.js')
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 
 module.exports = (envVars) => {
     const {env} = envVars
 
-    const envConfig = require(`./webpack.${env}.js`)
+    const smp = new SpeedMeasurePlugin();
 
+    const envConfig = require(`./webpack.${env}.js`)
     const config = merge(commonConfig, envConfig)
 
-    return config
+    return smp.wrap(config)
 }
