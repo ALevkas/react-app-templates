08989b5890e1337dbd876e855f3b60040408404f
allevkas <levkadev@gmail.com>
2021-12-28 16:46:26 +0300

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,41 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            tsx: true
-        }
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        'plugin:jsx-a11y/recommended',
-    ],
-    plugins: [
-        '@typescript-eslint',
-    ],
-    rules: {
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error']
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    env: {
-        browser: true,
-        es6: true,
-        node: true
+    'import/resolver': {
+      typescript: {},
     },
-    settings: {
-        react: {
-            version: 'detect'
-        },
-        'import/resolver': {
-            typescript: {
-            },
-        }
-    }
+  },
 }
--- src/client/components/Counter.tsx
+++ src/client/components/Counter.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
-
 const Counter = () => {
-    const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0)
 
-    const handleClick = () => {
-        setCounter(counter => counter + 1)
-    }
+  const handleClick = () => {
+    setCounter((counter) => counter + 1)
+  }
 
-    return <button onClick={handleClick}>You clicked me {counter} times</button>
+  return <button onClick={handleClick}>You clicked me {counter} times</button>
 }
 
 export default Counter
